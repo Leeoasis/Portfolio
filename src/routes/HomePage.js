a76e0faa6ca803f7b37522704b7c8b0b6541ac6b
Leslie <Kooliesk@gmail.com>
2023-09-08 20:20:25 +0200

    Configure home route

---
+++ src/routes/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import HomeImg from '../components/Home';
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <HomeImg />
+    </div>
+  )
+}
+
+export default HomePage
